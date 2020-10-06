1202bc83b80ba94bf122ef3fcf59fd0c32f070bf
Jacob Nielsen <jnillern@gmail.com>
2020-10-06 10:13:00 +0200

    Add temp

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
-import feathers from '@feathersjs/feathers';
-import '@feathersjs/transport-commons';
-import express from '@feathersjs/express';
-import socketio from '@feathersjs/socketio';
-import { v4 as uuidv4 } from 'uuid';
+import feathers from "@feathersjs/feathers";
+import "@feathersjs/transport-commons";
+import express from "@feathersjs/express";
+import socketio from "@feathersjs/socketio";
+import { v4 as uuidv4 } from "uuid";
 
 interface Device {
   id: string;
   mac: string;
-  status: 'Offline' | 'Online';
+  temp: number;
+  status: "Offline" | "Online";
   createdAt: Date;
   updatedAt: Date;
 }
@@ -19,20 +20,22 @@ class DeviceService {
     return this.devices;
   }
 
-  async create(data: Pick<Device, 'mac' | 'status'>) {
+  async create(data: Pick<Device, "mac" | "status" | "temp">) {
     const device = this.devices.find(({ mac }) => mac === data.mac);
     if (!device) {
       const newDevice: Device = {
         id: uuidv4(),
         mac: data.mac,
-        status: data.status || 'Offline',
+        status: data.status || "Offline",
+        temp: data.temp || 0,
         createdAt: new Date(),
         updatedAt: new Date(),
       };
       this.devices.push(newDevice);
       return device;
     } else {
-      device.status = 'Online';
+      device.status = "Online";
+      device.temp = data.temp;
       device.updatedAt = new Date();
       return device;
     }
@@ -45,14 +48,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 app.configure(express.rest());
 app.configure(socketio());
-app.use('/devices', new DeviceService());
+app.use("/devices", new DeviceService());
 app.use(express.errorHandler());
 
-app.on('connection', (connection) => app.channel('everybody').join(connection));
-app.publish((data) => app.channel('everybody'));
+app.on("connection", (connection) => app.channel("everybody").join(connection));
+app.publish((data) => app.channel("everybody"));
 
-app.listen(3030).on('listening', () => console.log('Feathers server listening on localhost:3030'));
+app
+  .listen(3030)
+  .on("listening", () =>
+    console.log("Feathers server listening on localhost:3030")
+  );
 
-app.service('devices').create({
-  mac: '0:80:e1:27:0:39',
+app.service("devices").create({
+  mac: "0:80:e1:27:0:39",
 });
